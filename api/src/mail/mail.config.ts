2d1b52478da5fdbc7e8337d6387addfb55cc439c
v <93080410+vynxc@users.noreply.github.com>
2025-04-24 18:26:16 -0400

    Update mail.config.ts

--- api/src/mail/mail.config.ts
+++ api/src/mail/mail.config.ts
@@ -1,7 +1,7 @@
 export const mailTransportConfig = {
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT ? parseInt(process.env.MAIL_PORT, 10) : 465,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
